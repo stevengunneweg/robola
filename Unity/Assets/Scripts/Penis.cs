6c40cf636fee9204dc6f2e78cfbde3bdaccaad6a
Gunnaway <stevengunneweg@gmail.com>
2015-01-23 22:48:31 +0100

    Added Map data structures

---
+++ Unity/Assets/Scripts/Penis.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Penis {
+
+	public int index = 0;
+
+	private Tile[,] tiles = new Tile[4, 4];
+
+	public Penis(int index)
+	{
+		this.index = index;
+		}
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
