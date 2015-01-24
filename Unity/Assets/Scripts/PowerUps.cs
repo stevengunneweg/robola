9c254aa72da9c6e1c00f1a3a7c8ecec13e08078d
Vincent Vu <vincent.vu@hva.nl>
2015-01-24 03:02:59 +0100

    Prefab gemaakt voor een tile, Tile kan geplaatst worden als power-up

--- Unity/Assets/Scripts/PowerUps.cs
+++ Unity/Assets/Scripts/PowerUps.cs
@@ -6,11 +6,16 @@ public class PowerUps : MonoBehaviour {
 	///<summary>
     /// collection of all the possible power ups
     ///</summary>
-
+    public GameObject tile;
     public int currentPowerUp;
 
     public void Blink(Rigidbody player, Vector3 input)
     {
         player.AddForce(input * 100000);
+    }
+
+    public void TileDrop(Rigidbody player)
+    {
+        GameObject tileDrop = Instantiate(tile, player.transform.position, transform.rotation) as GameObject;
     }
 }
