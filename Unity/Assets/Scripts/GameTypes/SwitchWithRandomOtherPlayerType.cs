71f75771c5413fb26e50b0a4475153a3caa83896
Rick Bunschoten <rick.bunschoten@hva.nl>
2015-01-24 22:16:03 +0100

    did some particle work on the players in menu and game scene, also the particles when you "swap" with another player

--- Unity/Assets/Scripts/GameTypes/SwitchWithRandomOtherPlayerType.cs
+++ Unity/Assets/Scripts/GameTypes/SwitchWithRandomOtherPlayerType.cs
@@ -7,12 +7,15 @@ public class SwitchWithRandomOtherPlayerType : GameType
     private PlayerType unluckyPerson; int person;
     private ArrayList nonInfectedPlayers = new ArrayList();
     private Vector3 playerPos, unluckyBastardsPos;
+    private ParticleSystem PS;
 
 
     protected override void UsePowerup(PlayerType player)
 	{
-		Sound sound = new Sound(transform.root.gameObject.audio, "Sounds/shift sfx");
+        nonInfectedPlayers.Clear();
+        PS = player.GetComponent<ParticleSystem>();
         SwitchWithUnluckyBastard(player, unluckyPersonToSwitchWith(player));
+		Sound sound = new Sound(transform.root.gameObject.audio, "Sounds/shift sfx");
     }
 
     PlayerType unluckyPersonToSwitchWith(PlayerType player)
@@ -27,13 +30,35 @@ public class SwitchWithRandomOtherPlayerType : GameType
             }
         }
 
-        person = Random.Range(0, nonInfectedPlayers.Count);
-        unluckyPerson = nonInfectedPlayers[person] as PlayerType;
-        return unluckyPerson;
+        if (nonInfectedPlayers.Count >= 1)
+        {
+            person = Random.Range(0, nonInfectedPlayers.Count);
+            unluckyPerson = nonInfectedPlayers[person] as PlayerType;
+            return unluckyPerson;
+        }
+        else
+        {
+            return null;
+        }
     }
 
     void SwitchWithUnluckyBastard(PlayerType player, PlayerType unluckyBastard)
     {
+        StartCoroutine(Particles(player, unluckyBastard));
+    }
+
+    IEnumerator Particles(PlayerType player, PlayerType unluckyBastard)
+    {
+        Debug.Log("test");
+        PS.startColor = new Color32(64, 179, 255, 255);
+        PS.startSize = 5f;
+        PS.startLifetime = 1f;
+        PS.Emit(15);
+        unluckyBastard.GetComponent<ParticleSystem>().startColor = new Color32(246, 129, 51, 255);
+        unluckyBastard.GetComponent<ParticleSystem>().startSize = 5f;
+        unluckyBastard.GetComponent<ParticleSystem>().startLifetime = 1f;
+        unluckyBastard.GetComponent<ParticleSystem>().Emit(15);
+        yield return new WaitForSeconds(.4f);
         playerPos = player.transform.position;
         unluckyBastardsPos = unluckyBastard.transform.position;
 
