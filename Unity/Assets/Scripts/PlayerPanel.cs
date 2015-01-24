74c492d9218e7b79a92f194d4135789f0d68f938
Bas Roding <bas.roding@gmail.com>
2015-01-24 11:28:58 +0100

    Added player panel ui

---
+++ Unity/Assets/Scripts/PlayerPanel.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class PlayerPanel : MonoBehaviour {
+
+	public PlayerType player;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(player.infected)
+		{
+			GetComponent<Image>().color = Color.green;
+		}
+	}
+}
