bb932d501171378c09daa268fdc12dc1e27cebcd
Gunnaway <stevengunneweg@gmail.com>
2015-01-24 12:36:03 +0100

    Added several new gametypes

---
+++ Unity/Assets/Scripts/GameTypes/RowShiftType.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RowShiftType : GameType {
+
+	private Map map;
+
+	protected override void Start () {
+		base.Start ();
+		map = FindObjectOfType<Map>();
+	}
+	
+	protected override void UsePowerup(PlayerType player)
+	{
+		Penis closest = map.FindClosest(player.transform.position);
+		for (int y = 0; y < map.penisses.GetLength(0); y++) {
+			for (int x = 0; x < map.penisses.GetLength(1); x++) {
+				if (map.penisses[y, x] == closest) {
+					if (Random.value >= 0.5) {
+						map.PushRow(y, Map.Direction.Left);
+					} else {
+						map.PushRow(y, Map.Direction.Right);
+					}
+				}
+			}
+		}
+	}
+}
---
+++ Unity/Assets/Scripts/GameTypes/TeleportType.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TeleportType : GameType {
+
+	private Map map;
+
+	protected override void Start () {
+		base.Start ();
+		map = FindObjectOfType<Map>();
+	}
+	
+	protected override void UsePowerup(PlayerType player)
+	{
+		float yPos = player.transform.position.y;
+		Vector3 newPos = map.RandomPenis ().transform.position;
+
+		//Todo: check if penis is occupied
+
+		player.transform.position = new Vector3(newPos.x, yPos, newPos.y);
+	}
+}
