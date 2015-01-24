6b798437d770b5d0320ece3ce9a66e2d48db2885
Bas Roding <bas.roding@gmail.com>
2015-01-24 15:01:45 +0100

    One power on at the time!

--- Unity/Assets/Scripts/Game.cs
+++ Unity/Assets/Scripts/Game.cs
@@ -13,14 +13,6 @@ public class Game : MonoBehaviour {
 	public GameObject InfectorWinPanel;
 	public GameObject PlayerWinPanel;
 
-	// Use this for initialization
-	void Start () {
-        GameType[] gametypes = FindObjectsOfType<GameType>();
-
-		int index = Random.Range(0, gametypes.Length-1);
-		gametypes[index].enabled = true;
-	}
-
 	void Update () {
 		GameObject.Find("GameTimer").GetComponent<Text>().text = time.ToString("f2");
 		if(FindObjectsOfType<PlayerType>().Where(p => !p.infected).ToList().Count == 0)
