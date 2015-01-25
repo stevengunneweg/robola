72d880449262932f438c43ae7d1bd970da36e0b2
madnessxd <brotherhoodofnod201@hotmail.com>
2015-01-25 14:07:53 +0100

    Audio + bugfix scene 2

--- Unity/Assets/Scripts/GameTypes/DropTileType.cs
+++ Unity/Assets/Scripts/GameTypes/DropTileType.cs
@@ -7,16 +7,15 @@ public class DropTileType : GameType {
     ParticleSystem particles;
 
     protected override void UsePowerup(PlayerType player)
-    {
+	{
         tile = Resources.Load("Tile") as GameObject;
-        particles = player.GetComponent<ParticleSystem>();/*
-		Sound sound = new Sound(transform.root.gameObject.audio, "Sounds/tile sfx");
-        */
-        StartCoroutine(TileDrop(player));
+        particles = player.GetComponent<ParticleSystem>();
+	    StartCoroutine(TileDrop(player));
     }
 
     public IEnumerator TileDrop(PlayerType currentPlayer)
-    {
+	{
+		Sound sound = new Sound(transform.root.gameObject.audio, "Sounds/tile sfx");
         particles.startColor = Color.red;
         particles.Emit(2);
         GameObject tileDrop = Instantiate(tile, currentPlayer.transform.position, Quaternion.EulerAngles(currentPlayer.transform.rotation.eulerAngles.x, currentPlayer.transform.rotation.eulerAngles.y + 180, currentPlayer.transform.rotation.eulerAngles.z)) as GameObject;
--- Unity/Assets/Scripts/GameTypes/SpeedUpType.cs
+++ Unity/Assets/Scripts/GameTypes/SpeedUpType.cs
@@ -6,15 +6,15 @@ public class SpeedUpType : GameType
     private TrailRenderer trail;
     protected override void UsePowerup(PlayerType player)
 	{
+		Sound sound = new Sound(transform.root.gameObject.audio, "Sounds/speed sfx");
         trail = player.GetComponent<TrailRenderer>();
         StartCoroutine(GottaGoFast(player));
-		//Sound sound = new Sound(transform.root.gameObject.audio, "Sounds/speed sfx");
-        player.uninfectedSpeed = 12;
-        
+        player.uninfectedSpeed = 12;        
     }
 
     IEnumerator GottaGoFast(PlayerType player)
-    {        
+	{        
+		Sound sound = new Sound(transform.root.gameObject.audio, "Sounds/speed sfx");
         trail.enabled = true;
         yield return new WaitForSeconds(player.duration);
         trail.enabled = false;
