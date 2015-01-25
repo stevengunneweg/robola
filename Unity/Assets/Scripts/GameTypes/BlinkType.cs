c16352c6b726c13869c86c720a99580ed93b18c2
Rick Bunschoten <rick.bunschoten@hva.nl>
2015-01-25 12:55:22 +0100

    blink out of map solution, once outside you get teleported back to spawn position

--- Unity/Assets/Scripts/GameTypes/BlinkType.cs
+++ Unity/Assets/Scripts/GameTypes/BlinkType.cs
@@ -2,10 +2,21 @@
 using System.Collections;
 
 public class BlinkType : GameType {
+    private TrailRenderer TR;
+    private ParticleSystem PS;
 
     protected override void UsePowerup(PlayerType player)
     {
-		player.transform.position += (player.player.input * 5);
+        TR = player.GetComponent<TrailRenderer>();
+        StartCoroutine(trail(player));
 		Sound sound = new Sound(transform.root.gameObject.audio, "Sounds/blink sfx");
+    }
+
+    IEnumerator trail(PlayerType player)
+    {
+        TR.enabled = true;
+        player.transform.position += (player.player.input * 5);
+        yield return new WaitForSeconds(2f);
+        TR.enabled = false;
     }
 }
