5476e3b7afe656746b201bb627e0f82377703dc4
Rick Bunschoten <rick.bunschoten@hva.nl>
2015-01-25 03:53:51 +0100

    added a pre-countdown

---
+++ Unity/Assets/showPowerUp.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class showPowerUp : MonoBehaviour {
+    private Text _text;
+    private GameType[] types;
+
+    void Awake()
+    {
+        _text = GetComponent<Text>();
+        types = FindObjectsOfType<GameType>();
+    }
+
+    void Update()
+    {
+        foreach (GameType type in types)
+        {
+            if (type.enabled)
+            {
+                _text.text = type.GetType().Name;
+                break;
+            }
+        }
+    }
+}
